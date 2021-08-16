5c646c8a7d4afa2625d94c8d1a0533c08452ce5b
Michal Moskal <michal@moskal.me>
2021-08-16 13:29:45 -0700

    Build fixes

--- test.ts
+++ test.ts
@@ -2,13 +2,13 @@ namespace userconfig {
 }
 
 console.addListener((pri, txt) => control.dmesg("C: " + txt.slice(0, -1)))
-jacdac.consolePriority = ConsolePriority.Log
+jacdac.logPriority = ConsolePriority.Log
 
 control.dmesg("Hello")
 
 pins.A9.digitalWrite(false)
 
-jacdac.deviceNamer.start()
+jacdac.roleManagerServer.start()
 jacdac.ml4fHost.start()
 
 jacdac.start()
